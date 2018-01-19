db9fd15af40157a23bf056c8a934e0bd9307c157
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-19 18:03:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {hello: "song1", there: "song2"};
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
 }
+
+function removeFromPlaylist(playlist, artistName, songTitle) {
+  
+}
